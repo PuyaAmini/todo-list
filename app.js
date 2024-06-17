855332f00e751278edb5b44cf039d8629ab5cd97
pamini <puyagit@gmail.com>
2024-06-18 00:42:51 +0330

    js => add func

---
+++ app.js
@@ -0,0 +1,20 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+
+// adding new tasks to the list:
+const li_generator = (todo) => {
+  const li_html = `<li>
+          <span>${todo}</span>
+          <img class="delete" src="./src/delete.png" alt="delete" />
+        </li>`;
+  list.innerHTML += li_html;
+};
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    li_generator(todo);
+    addForm.reset();
+  }
+});
